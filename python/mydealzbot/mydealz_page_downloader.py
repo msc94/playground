57061ffbefbb3e51fd3752a6d314999ceedc68db
Marcel Schneider <marcel.schneider5@freenet.de>
2019-07-28 10:30:09 +0200

    Add timing to http requests

--- python/mydealzbot/mydealz_page_downloader.py
+++ python/mydealzbot/mydealz_page_downloader.py
@@ -2,14 +2,23 @@ import aiohttp
 import asyncio
 import logging
 import sys
+import time
 
 class MydealzPageDownloader(object):
     def __init__(self):
         self._session = aiohttp.ClientSession()
+        self._total_time = 0.0
 
     async def _fetch(self, url):
+        start = time.time()
         async with self._session.get(url) as response:
-            return await response.text()
+            result = await response.text()
+
+            request_time = (time.time() - start)
+            self._total_time += request_time
+            logging.debug(f"It took {request_time} to get {url}. Total time up to now: {self._total_time}")
+            
+            return result
 
     async def mydealz_index(self, page_num):
         return await self._fetch(f"https://www.mydealz.de/?page={page_num}")
