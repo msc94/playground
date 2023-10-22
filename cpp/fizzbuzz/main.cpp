87791b203df0c61b240bb65cf6addfd7df5222fd
msc94 <msc94@users.noreply.github.com>
2023-10-22 20:39:25 +0200

    fizzbuzz: 457MiB/s

--- cpp/fizzbuzz/main.cpp
+++ cpp/fizzbuzz/main.cpp
@@ -3,18 +3,41 @@
 #include <cstdint>
 #include <iostream>
 
+#include <string>
+#include <unistd.h>
+
 constexpr std::size_t iterations = 500'000'000;
 
+constexpr std::size_t buffer_size = 1024 * 1024;
+
+void write(const std::string &str, std::string &buffer) {
+  if (buffer.size() + str.size() > buffer_size) {
+    if (write(STDOUT_FILENO, buffer.data(), buffer.size()) != buffer.size()) {
+      exit(1);
+    }
+    buffer.clear();
+    if (buffer.capacity() != buffer_size) {
+      exit(1);
+    }
+  }
+
+  buffer.insert(buffer.end(), str.begin(), str.end());
+  buffer.insert(buffer.end(), '\n');
+}
+
 int main() {
+  std::string buffer;
+  buffer.reserve(buffer_size);
+
   for (std::size_t i = 1; i <= iterations; i++) {
     if (i % 5 == 0 && i % 3 == 0) {
-      std::cout << "FizzBuzz\n";
+      write("FizzBuzz", buffer);
     } else if (i % 5 == 0) {
-      std::cout << "Buff\n";
+      write("Buff", buffer);
     } else if (i % 3 == 0) {
-      std::cout << "Fizz\n";
+      write("Fizz", buffer);
     } else {
-      std::cout << i << "\n";
+      write(std::to_string(i), buffer);
     }
   }
 }
